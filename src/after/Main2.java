caf993f03423befd307f6fea61a6981a104f1bde
sbrace <sbrace0822@gmail.com>
2023-09-22 10:16:03 +0800

    update dropSameItem

--- src/after/Main2.java
+++ src/after/Main2.java
@@ -7,6 +7,7 @@ public class Main2 {
     public static void main(String[] args) {
         Main2 main = new Main2();
         long start = System.currentTimeMillis();
+        System.out.println("start...");
         main.mergeRelationLogs2();
         long end = System.currentTimeMillis();
         System.out.println("time = " + (end - start) / 1000.0F + "s");
@@ -17,34 +18,35 @@ public class Main2 {
 
     static {
         //init
-        int size = 50000;
+        System.out.println("init...");
+        int size = 5_000_000;
         for (int i = 0; i < size; i++) {
             relationLogs.add(RelationLog2.create());
         }
     }
 
     private void mergeRelationLogs2() {
-        relationLogs = relationLogs.stream().collect(Collectors.groupingBy(RelationLog2::groupValue))
+        relationLogs = relationLogs.stream()
+                .collect(Collectors.groupingBy(RelationLog2::groupValue))
                 .values()
                 .stream()
-                .map(this::dropSameItem)
+                .peek(this::dropSameItem)
                 .flatMap(List::stream)
                 .collect(Collectors.toCollection(LinkedList::new));
     }
 
-    private List<RelationLog2> dropSameItem(List<RelationLog2> list) {
-        for (int i = 0; i < list.size(); i++) {
+    private void dropSameItem(List<RelationLog2> list) {
+        out: for (int i = 0; i < list.size() - 1; i++) {
             RelationLog2 outRelationLog2 = list.get(i);
             for (int j = i + 1; j < list.size(); j++) {
                 RelationLog2 inRelationLog2 = list.get(j);
-                if (outRelationLog2.isReversedLog(inRelationLog2)) {
+                if (outRelationLog2.isReversedLog(inRelationLog2)){
                     list.remove(j);
                     list.remove(i);
-                    dropSameItem(list);
-                    return list;
+                    i--;
+                    continue out;
                 }
             }
         }
-        return list;
     }
 }
